ab8411bdee4f3c12239b1f2e73779b3b8db362a3
Cameron Ryan <125604130+CRyan-EPIC@users.noreply.github.com>
2024-06-28 17:23:57 -0600

    Update lab_install_ubuntu.sh

--- lab_install_ubuntu.sh
+++ lab_install_ubuntu.sh
@@ -47,10 +47,10 @@ cd jetbrains-toolbox-2.3.0.30876
 ./jetbrains-toolbox
 cd ..
 
-#Burpsuite
-wget https://portswigger.net/burp/releases/community/latest
-chmod 700 ./burpsuite_community_linux_v*.sh
-./burpsuite_community_linux_v*.sh
+#Burpsuite must do it manually
+#Go to https://portswigger.net/burp/releases/community/latest
+#chmod 700 ./burpsuite_community_linux_v*.sh
+#./burpsuite_community_linux_v*.sh
 
 #UEFI Dual Boot Screen
 #git clone https://github.com/vinceliuice/grub2-themes.git
@@ -60,7 +60,7 @@ chmod 700 ./burpsuite_community_linux_v*.sh
 
 #Chrome
 wget https://dl-ssl.google.com/linux/linux_signing_key.pub -O /tmp/google.pub
-gpg --no-default-keyring --keyring /etc/apt/keyrings/google-chrome.gpg --import /tmp/google.pub
+sudo gpg --no-default-keyring --keyring /etc/apt/keyrings/google-chrome.gpg --import /tmp/google.pub
 echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get update 
 sudo apt-get install google-chrome-stable -y
