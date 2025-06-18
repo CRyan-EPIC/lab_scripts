e64862bb9f0682e5f59aff4fc5bbd0a2ea4b6241
Cameron Ryan <125604130+CRyan-EPIC@users.noreply.github.com>
2025-06-18 13:08:18 -0600

    Update lab_install_fedora.sh

--- lab_install_fedora.sh
+++ lab_install_fedora.sh
@@ -9,7 +9,7 @@ sudo dnf -y install openssh-server openvpn blender krita godot3 gimp \
   gcc g++ make lolcat gparted filezilla obs-studio \
   fish zsh xrdp remmina make flatpak wireshark timeshift traceroute python3 \
   nmap zip gzip bzip2 wget curl unzip ufw tree minicom git neofetch \
-  git cpufetch wget curl exfatprogs podman flatpak pip
+  git cpufetch wget curl exfatprogs podman flatpak pip polkit -y
 
 #snap for Fedora
 #sudo dnf install snapd -y
@@ -24,7 +24,7 @@ sudo systemctl enable libvirtd
 #Installing dysk https://github.com/Canop/dysk?tab=readme-ov-file
 cd /usr/bin
 sudo wget https://dystroy.org/dysk/download/x86_64-linux/dysk
-chmod 755 ./dysk
+sudo chmod 755 ./dysk
 cd ~
 
 #Install webcamize https://github.com/cowtoolz/webcamize?tab=readme-ov-file#installation
@@ -73,72 +73,3 @@ sudo cp /etc/dnf/dnf.conf /etc/dnf/dnf.conf.backup
 sudo echo "exclude=steam" >> /etc/dnf/dnf.conf
 
 
-###########Allowing the user to change the IP without a password - not tested
-sudo dnf install polkit
-
-# Define the policy directory
-POLICY_DIR="/etc/polkit-1/localauthority/50-local.d"
-
-# Create the directory if it does not exist
-sudo mkdir -p "$POLICY_DIR"
-
-# Define the policy file
-POLICY_FILE="$POLICY_DIR/99-allow-network-changes.pkla"
-
-# Create or overwrite the policy file
-sudo bash -c 'cat > "$POLICY_FILE" <<EOF
-[Allow all users to manage network]
-Identity=unix-user:*
-Action=org.freedesktop.NetworkManager.network-control
-ResultAny=yes
-ResultInactive=yes
-ResultActive=yes
-EOF'
-
-echo "Policy file created at $POLICY_FILE"
-
-#!/bin/bash
-
-# Get the username (default to the current user)
-USER_TO_MODIFY=${1:-$USER}
-
-# Detect the OS
-if grep -q "Ubuntu" /etc/os-release; then
-    OS="Ubuntu"
-    GROUP="netdev"
-elif grep -q "Fedora" /etc/os-release; then
-    OS="Fedora"
-    GROUP="wheel"
-else
-    echo "Unsupported OS. This script works on Ubuntu and Fedora only."
-    exit 1
-fi
-
-echo "Detected OS: $OS"
-
-echo "Adding $USER_TO_MODIFY to $GROUP group..."
-sudo usermod -aG $GROUP $USER_TO_MODIFY
-
-# Update sudoers file
-echo "Updating sudoers for network commands..."
-SUDOERS_FILE="/etc/sudoers.d/network_privileges"
-echo "%$GROUP ALL=(ALL) NOPASSWD: /usr/sbin/ip, /usr/bin/nmcli, /usr/sbin/netplan" | sudo tee $SUDOERS_FILE > /dev/null
-sudo chmod 440 $SUDOERS_FILE
-
-# Configure Polkit for NetworkManager
-echo "Configuring Polkit rules..."
-POLKIT_FILE="/etc/polkit-1/localauthority/50-local.d/org.freedesktop.NetworkManager.pkla"
-sudo tee $POLKIT_FILE > /dev/null <<EOF
-[Allow NetworkManager]
-Identity=unix-group:$GROUP
-Action=org.freedesktop.NetworkManager.*
-ResultAny=yes
-ResultInactive=yes
-ResultActive=yes
-EOF
-
-# Restart polkit
-echo "Restarting polkit..."
-sudo systemctl restart polkit
-
-echo "Configuration complete! Please log out and log back in for changes to take effect."
